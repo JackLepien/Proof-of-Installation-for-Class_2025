51a437361371d3eb98b2b749744800b0bfbbf684
JackLepien <jacklepien@gmail.com>
2026-01-14 05:57:13 -0600

    Init Commit
    Added basic p5.js project

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  textSize(12);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text("My cat is sitting on my ankle and it hurts.", 200, 200);
+  text("Please help me.", 200, 220);
+}
